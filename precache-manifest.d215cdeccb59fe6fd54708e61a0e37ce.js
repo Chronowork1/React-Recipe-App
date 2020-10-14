07871fa0ccec87e9872b2db0d665a36395e57916
Chronowork1 <jianxie6464@gmail.com>
2020-10-14 02:13:49 -0700

    Updates

---
+++ precache-manifest.d215cdeccb59fe6fd54708e61a0e37ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bb77988e8b17db86a4c41a711bf934d",
+    "url": "/React-Recipe-App/index.html"
+  },
+  {
+    "revision": "5ef6922452cf83bebf9c",
+    "url": "/React-Recipe-App/static/css/main.2e8634c7.chunk.css"
+  },
+  {
+    "revision": "c75e708c4b4cef3e4fd8",
+    "url": "/React-Recipe-App/static/js/2.14d0c068.chunk.js"
+  },
+  {
+    "revision": "5ef6922452cf83bebf9c",
+    "url": "/React-Recipe-App/static/js/main.931d8ee0.chunk.js"
+  },
+  {
+    "revision": "7f84c57f501de3f2b3ed",
+    "url": "/React-Recipe-App/static/js/runtime-main.fb6134f1.js"
+  }
+]);
